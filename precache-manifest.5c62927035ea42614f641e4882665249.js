53cc1a674864830902311a31d26138059780635e
Bril Mikhail <Kolabrod@mail.ru>
2020-03-25 16:43:45 +0300

    Updates

---
+++ precache-manifest.5c62927035ea42614f641e4882665249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05a7acb7e8b7fe7b51148ed7925f2bf",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "608c8f11a8ca3d4d2a25",
+    "url": "/weather-react/static/css/main.64d124e4.chunk.css"
+  },
+  {
+    "revision": "79e679bbd202b3158b8d",
+    "url": "/weather-react/static/js/2.4f629c2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.4f629c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608c8f11a8ca3d4d2a25",
+    "url": "/weather-react/static/js/main.bd2b511b.chunk.js"
+  },
+  {
+    "revision": "7b9fd889ed9a27a7c426",
+    "url": "/weather-react/static/js/runtime-main.f530af41.js"
+  },
+  {
+    "revision": "2a870e8efa78578c630e190d84a54380",
+    "url": "/weather-react/static/media/weather.2a870e8e.jpg"
+  }
+]);
